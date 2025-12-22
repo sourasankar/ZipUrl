a6dd103cf090f419ab3d279005fe2a512aeef282
sourasankar <64581748+sourasankar@users.noreply.github.com>
2025-12-22 11:44:55 +0530

    First commit for Development , Spring initializer

---
+++ src/main/java/com/zipurl/core/ZipurlApplication.java
@@ -0,0 +1,13 @@
+package com.zipurl.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipurlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zipurl/core/ZipurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipurl.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
