a21822a195c0b85c3e0a8d55fb90e84c420293ef
sourasankar <64581748+sourasankar@users.noreply.github.com>
2025-12-30 21:04:01 +0530

    changes

--- src/main/java/com/zipurl/core/service/impl/DatabaseServiceImpl.java
+++ src/main/java/com/zipurl/core/service/impl/DatabaseServiceImpl.java
@@ -3,6 +3,7 @@ package com.zipurl.core.service.impl;
 import com.zipurl.core.dao.ShortUrlRepo;
 import com.zipurl.core.entity.ShortUrl;
 import com.zipurl.core.service.DatabaseService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ public class DatabaseServiceImpl implements DatabaseService {
     private final ShortUrlRepo shortUrlRepo;
     private final RedisWriteServiceImpl redisWriteService;
 
+    @CircuitBreaker(name = "dbError", fallbackMethod = "fallbackDBMethod")
     @Override
     public String getFromDBAndCacheToRedis(String key) {
         System.out.println("Inside getFromDBAndCacheToRedis");
@@ -28,20 +30,27 @@ public class DatabaseServiceImpl implements DatabaseService {
         return null;
     }
 
+    public String fallbackDBMethod(String key, Throwable e) {
+        System.out.println("Inside fallbackDBMethod : " + e);
+        return null;
+    }
+
+    @CircuitBreaker(name = "dbError", fallbackMethod = "fallbackDBMethod")
     @Override
     public Boolean saveToDBAndCacheToRedis(String shortId, String longUrl) {
         System.out.println("Inside saveToDBAndCacheToRedis");
         ShortUrl entity = new ShortUrl();
         entity.setShortId(shortId);
         entity.setUrl(longUrl);
-        try {
-            shortUrlRepo.saveAndFlush(entity);
-            Boolean redisFlag = redisWriteService.saveUrlWithExpiry(shortId, longUrl);
-            System.out.println("saveToDB Redis save status : " + redisFlag);
-        } catch (Exception e) {
-            System.out.println("DB save failure : " + e);
-            return false;
-        }
+        shortUrlRepo.saveAndFlush(entity);
+        Boolean redisFlag = redisWriteService.saveUrlWithExpiry(shortId, longUrl);
+        System.out.println("saveToDB Redis save status : " + redisFlag);
         return true;
     }
+
+    public Boolean fallbackDBMethod(String shortId, String longUrl, Throwable e) {
+        System.out.println("Inside fallbackDBMethod : " + e);
+        return false;
+    }
+
 }
